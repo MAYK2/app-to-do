6fb167a04bc6d6733e3d213307cf3361417dc6b4
Mayco Dominguez <maycodominguez123@gmail.com>
2024-09-02 14:39:02 -0300

    arrancando proyecto nuevo

---
+++ src/test/java/taskapp/app_to_do/AppToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package taskapp.app_to_do;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
